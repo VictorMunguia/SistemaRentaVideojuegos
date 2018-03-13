8a5490ac65134c8c5529f12e532fc9e009d1e74f
VictorMunguia <victor00171@hotmail.com>
2018-03-13 01:14:47 -0600

    Problema con la base de datos en el metodo modificar

--- RentaVideojuegosMVC5/RentaVideojuegosMVC5/Controllers/VideojuegosController.cs
+++ RentaVideojuegosMVC5/RentaVideojuegosMVC5/Controllers/VideojuegosController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using RentaVideojuegosMVC5.Models;
 using RentaVideojuegosMVC5.ViewModel;
 using System.Data.Entity;
 using System.Net;
@@ -106,6 +105,38 @@ namespace RentaVideojuegosMVC5.Controllers
                 return View(ViewModelVideojuego);
             }
            
+            
+        }
+
+        public ActionResult ModificarInformacion(Videojuego Videojuego)
+        {
+            if (!ModelState.IsValid)
+            {
+                var ViewModel = new VideojuegosViewModel
+                {
+                    Videojuego = Videojuego,
+                    ListaGenero = _context.GenerosBaseDatos.ToList(),
+                    ListaEdad = _context.EdadesBaseDatos.ToList()
+                    
+
+                };
+
+                return View("Modificar", ViewModel);// Esta condicion compara si es valido todo esto, en caso de que sea diferente de valido returnaremos al usuario al formulario con todo lo que a llenado pero imprimiremos los mensajes de error.
+            }
+           
+            
+                var VideojuegoBaseDeDatos = _context.VideojuegosBaseDatos.Single(c => c.Id == Videojuego.Id);
+
+                VideojuegoBaseDeDatos.Nombre = Videojuego.Nombre;
+                VideojuegoBaseDeDatos.Descripcion = Videojuego.Descripcion;
+                VideojuegoBaseDeDatos.FechaDePublicacion = Videojuego.FechaDePublicacion;
+                VideojuegoBaseDeDatos.NumeroDisponiblre = Videojuego.NumeroDisponiblre;
+                VideojuegoBaseDeDatos.EdadId = Videojuego.EdadId;
+                VideojuegoBaseDeDatos.GeneroId = Videojuego.GeneroId;
+
+                _context.SaveChanges();
+
+                return RedirectToAction(Convert.ToString(Detalles(VideojuegoBaseDeDatos.Id)), "Videojuegos");
             
         }
     }
